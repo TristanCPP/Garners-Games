400f6affef352a22d5006e7ad82cb02d18cbc494
Tristan <gtristan23@yahoo.com>
2024-07-29 14:53:52 -0500

    Added back database connection files

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+//define("db_host", "localhost");
+//define("db_user", "root");
+//define("db_pass", "");
+//define("db_name", "testdb");
+
+define("db_host", "npcwp.com");
+define("db_user", "npcwpcom_tristan");
+define("db_pass", "npcwp4018%");
+define("db_name", "npcwpcom_tristanDB");
+
+?>
